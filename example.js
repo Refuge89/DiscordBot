0774b1a34b5488b4fa0e53d836d475cf4b17b848
Exitare <exi@pihaar.de>
2017-02-11 17:59:49 +0100

    Add new Commands

--- example.js
+++ example.js
@@ -21,16 +21,33 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
     }
 
 if (e.message.content == "!commands" || e.message.content == "!Commands" ){
-    e.message.channel.sendMessage("All available commands are: \n!Ping \n!Pong \n!homepage");
+    e.message.channel.sendMessage("All available commands are: \n!Ping \n!Pong \n!Homepage \n!Commands \n!Help");
 }
 
 if (e.message.content == "!homepage" || e.message.content == "!Homepage"){
     e.message.channel.sendMessage("avarius.net");
 }
 
+if (e.message.content == "!help" || e.message.content == "!Help"){
+    e.message.channel.sendMessage("What is type of needed help?");
+}
+
+if (e.message.content == "!wow" || e.message.content == "!WOW"){
+    e.message.channel.sendMessage("All WoW related commands are: \n!wow realmlist \n!wow rates \n!wow features");
+}
+
+if (e.message.content == "!wow realmlist"){
+    e.message.channel.sendMessage("For our WotLK and Legion realm the realmlist is \nlogon.wow.blacknetwork.eu \n#################### \nFor our MOP Realm the realmlist is \nlogon.wow.blacknetwork.eu:3725.");
+}
+
+
+if (e.message.content == "!wow rates"){
+    e.message.channel.sendMessage("Blizzlike Realms Rates are: \nXP-Rate: Not available due redesign of the Realm!");
+}
+
 });
 
 
 client.Dispatcher.on("GUILD_MEMBER_ADD", e => {
-    e.message.channel.sendMessage("Welcome on our Server! \nType !commands to get a List of all available Commands! \nIf you need Support, please use our SupportChanel");
+    e.message.channel.sendMessage("Welcome on our Server! \nType !commands to get a List of all available Commands! \nIf you need Support, please use our SupportChannel");
 });
