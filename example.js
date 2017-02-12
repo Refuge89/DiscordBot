f984d27d95dda3cb7cd2a93cfaa7631642d5da1a
Exitare <exi@pihaar.de>
2017-02-12 14:18:31 +0100

    New Version

--- example.js
+++ example.js
@@ -6,6 +6,7 @@ client.connect({
   token: "Mjc5OTQ3Mzk1MTc1ODA5MDI1.C4CuwQ.M53C-HsnBUoGTGb5InMt-8dVqsg"
 });
 
+
 client.Dispatcher.on("GATEWAY_READY", e => {
     console.log("Connected as: " + client.User.username);
 });
@@ -21,7 +22,7 @@ client.Dispatcher.on("MESSAGE_CREATE", e => {
     }
 
 if (e.message.content == "!commands" || e.message.content == "!Commands" ){
-    e.message.channel.sendMessage("All available commands are: \n!Ping \n!Pong \n!Homepage \n!Commands \n!Help");
+    e.message.channel.sendMessage("All available commands are: \n!Ping \n!Pong \n!Homepage \n!Commands \n!Help \n!wow \n!metin \nark \n!team");
 }
 
 if (e.message.content == "!homepage" || e.message.content == "!Homepage"){
@@ -29,15 +30,16 @@ if (e.message.content == "!homepage" || e.message.content == "!Homepage"){
 }
 
 if (e.message.content == "!help" || e.message.content == "!Help"){
-    e.message.channel.sendMessage("What is type of needed help?");
+    e.message.channel.sendMessage("What is the type of needed help? Type in one of the following Keyworks! \n!wow \n!metin2 \n!ark");
 }
 
+//wow help commands
 if (e.message.content == "!wow" || e.message.content == "!WOW"){
     e.message.channel.sendMessage("All WoW related commands are: \n!wow realmlist \n!wow rates \n!wow features");
 }
 
 if (e.message.content == "!wow realmlist"){
-    e.message.channel.sendMessage("For our WotLK and Legion realm the realmlist is \nlogon.wow.blacknetwork.eu \n#################### \nFor our MOP Realm the realmlist is \nlogon.wow.blacknetwork.eu:3725.");
+    e.message.channel.sendMessage("For our WotLK and Legion realm the realmlist is: \nlogon.wow.blacknetwork.eu \n\n#################### \n\nFor our MOP Realm the realmlist is \nlogon.wow.blacknetwork.eu:3725.");
 }
 
 
@@ -46,6 +48,17 @@ if (e.message.content == "!wow rates"){
 }
 
 });
+
+//metin2
+if (e.message.content == "!metin" || e.message.content == "!Metin"){
+    e.message.channel.sendMessage("All Metin related commands are: \n!metin realmlist \n!metin rates \n!metin features");
+}
+
+
+//ark evolved
+if (e.message.content == "!ark" || e.message.content == "!Ark"){
+    e.message.channel.sendMessage("All Ark related commands are: \n!ark connect");
+}
 
 
 client.Dispatcher.on("GUILD_MEMBER_ADD", e => {
